acb8b99778116b99fdc1b53072998907c9eb7573
Elton H. Choi <babchol@gmail.com>
2016-11-04 06:35:32 +0900

    update acrobot_control.py
    try noncollocated pfl,
    have to solve to subscribe joint states of acrobot

--- ros/acrobot/acrobot_control/scripts/acrobot_control.py
+++ ros/acrobot/acrobot_control/scripts/acrobot_control.py
@@ -5,12 +5,84 @@ import math
 
 from std_msgs.msg import Float64
 from math import sin,cos,atan2,sqrt,fabs
+from numpy import *
+from rospy.numpy_msg import numpy_msg
+
+class Acrobot:
+    # kinematic, dynamic parameter
+    m1 = 1
+    m2 = 1
+    I1 = 1
+    I2 = 1
+    l1 = 1
+    l2 = 1
+    lc1 = 0.5
+    lc2 = 0.5
+    H = zeros((2,2))
+    C = zeros((2,2))
+    G = zeros((2,1))
+    # state
+    q = zeros((2,1))
+    q_old = zeros((2,1))
+    dq = zeros((2,1))
+    
+    # desired value
+    ddq1_d = 0
+    dq1_d = 0
+    q1_d = pi
+
+    # torque
+    tau2 = 0.0
+
+    # gain
+    kp = 1
+    kv = 0.2
+
+    def inv_dynamics(self, q, dq):
+        g = 9.81
+        self.H[0,0] = self.I1 + self.I2 + self.m2*self.l1*self.l1 + 2*self.m2*self.l1*self.lc2*cos(self.q[1])
+        self.H[0,1] = self.I2 + self.m2*self.l1*self.lc2*cos(self.q[1])
+        self.H[1,0] = self.I2 + self.m2*self.l1*self.lc2*cos(self.q[1])
+        self.H[1,1] = self.I2
+
+        self.C[0,0] = -2*self.m2*self.l1*self.lc2*sin(self.q[1])*self.dq[1]
+        self.C[0,1] = -self.m2*self.l1*self.lc2*sin(self.q[1])*self.dq[1]
+        self.C[1,0] = self.m2*self.l1*self.lc2*sin(self.q[1])*self.dq[0]
+        self.C[1,1] = 0
+
+        self.G[0] = self.m1*g*self.lc1*sin(self.q[0]) + self.m2*g*(self.l1*sin(self.l1*sin(self.q[0]) + self.lc2*sin(self.q[0] + self.q[1])))
+        self.G[1] = self.m2*g*self.lc2*sin(self.q[0] + self.q[1])
+
+    def swingup_control(self):
+        self.dq = self.q - self.q_old
+        
+        self.inv_dynamics(self.q, self.dq)
+        phi = self.C.dot(self.dq) + self.G
+        v = self.ddq1_d - self.kv*(self.dq1_d - self.dq[0]) - self.kp*(self.q1_d - self.q[0])
+        
+        self.tau2 = ( self.H[1,0] - self.H[1,1]*self.H[0,0]/self.H[0,1] ) * v - self.H[1,1]/self.H[0,1]*phi[0] + phi[1]
+        #print( phi)
+        self.q_old = self.q
+
+        return self.tau2
+
+        def lqr_control(self):
+            pass
+
+#Joint state publisher
+def acrobot_joint_state_subscriber(data):
+    acrobot.q[0] = data[0]
+    acrobot.q[1] = data[1]
+    print(acrobot.q)
 
 #Define a RRBot joint positions publisher for joint controllers.
 def acrobot_control_publisher():
 
     #Initiate node for controlling joint1 and joint2 positions.
     rospy.init_node('acrobot_control_node', anonymous=True)
+
+    #Define subscribers for each joint state
+    rospy.Subscriber('rrbot/joint_states/position', numpy_msg(Float64), acrobot_joint_state_subscriber)
 
     #Define publishers for each joint position controller commands.
     pub2 = rospy.Publisher('/rrbot/joint2_torque_controller/command', Float64, queue_size=10)
@@ -21,11 +93,14 @@ def acrobot_control_publisher():
     i = 0
     while not rospy.is_shutdown():
 
-        #Have each joint follow a sine movement of sin(i/100).
-        sine_movement = sin(i/100.)
+        # calculate torque
 
-        #Publish the same sine movement to each joint.
-        pub2.publish(sine_movement)
+        acrobot.swingup_control()
+
+        #Publish torque
+        pub2.publish(10*sin(100*i))
+        #print(acrobot.tau2)
+        #pub2.publish(1.0)
 
         i = i+1 #increment i
 
@@ -33,5 +108,6 @@ def acrobot_control_publisher():
 
 #Main section of code that will continuously run unless rospy receives interuption (ie CTRL+C)
 if __name__ == '__main__':
+    acrobot = Acrobot()
     try: acrobot_control_publisher()
     except rospy.ROSInterruptException: pass
